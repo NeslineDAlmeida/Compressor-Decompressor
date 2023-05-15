aabb796d267a99ea9a94848204ff681889e2fd7d
nesline <73271701+nesline@users.noreply.github.com>
2023-05-15 20:12:13 +0530

    Add files via upload

---
+++ Compressor_Decompressor/src/main/java/comp_decomp/decompressor.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package comp_decomp;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.zip.GZIPInputStream;
+
+/**
+ *
+ * @author nesli
+ */
+public class decompressor {
+    
+    String extension ;
+    public static void method(File file) throws IOException{
+         String fileDirectory = file.getParent();
+        
+        FileInputStream fis = new FileInputStream(file);
+
+        GZIPInputStream qzip = new GZIPInputStream(fis);
+        
+        FileOutputStream fos = new FileOutputStream(fileDirectory+"/DecompressedFile.");
+
+        
+        
+        byte[] buffer = new byte[1024];
+        int len;
+        while((len=qzip.read(buffer))!=-1){
+            fos.write(buffer,0,len);
+        }
+
+        qzip.close();
+        fos.close();
+        fis.close();
+
+    }
+    
+}
